184dd382a974e0c9467b3e52a7b2b8dca8430c9c
jumdm-019 <juliadacorregio@gmail.com>
2026-09-25 19:44:03 -0300

    feat: implementa controller de autores

---
+++ src/controllers/AutorController.ts
@@ -0,0 +1,30 @@
+import { AutorService } from "../services/AutorService";
+import { Autor } from "../models/Autor";
+
+export class AutorController {
+    private autorService: AutorService
+
+    constructor(){
+        this.autorService = new AutorService()
+    }
+
+    async criar(nome: string): Promise<void> {
+        await this.autorService.criar(nome)
+    }
+
+    async listar(): Promise<Autor[]> {
+        return await this.autorService.listar()
+    }
+
+    async buscaPorId(id: number): Promise<Autor> {
+        return await this.autorService.buscarPorId(id)
+    }
+
+    async atualizar(id: number, nome: string): Promise<void>{
+        await this.autorService.atualizar(id, nome)
+    }
+
+    async excluir(id: number): Promise<void>{
+        await this.autorService.excluir(id)
+    }
+}
